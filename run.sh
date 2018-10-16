2cea5217d1a91875325112e7934380a573a520ad
niuyu <niuyush@sinosoft.com.cn>
2018-10-16 10:50:06 +0800

    初始化

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
 
+sed -i "s/%zoo_servers%/${ZOO_SERVERS}/" /app/conf/zoo.cfg
+
 zkServer.sh start /app/conf/zoo.cfg
